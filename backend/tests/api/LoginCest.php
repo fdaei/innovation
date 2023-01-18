84ccd255a81b1a3de8f462e095947b60597daa37
fdaei <nasimdaei8@gmail.com>
2023-01-18 14:44:44 +0330

    test

---
+++ backend/tests/api/LoginCest.php
@@ -0,0 +1,17 @@
+<?php
+namespace backend\tests\api;
+
+use backend\tests\ApiTester;
+
+class LoginCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+
+    }
+}
