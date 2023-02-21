e2972e633a333b67751207a0150530009f01fcbe
fdaei <nasimdaei8@gmail.com>
2023-02-21 17:47:06 +0330

    view notification

--- backend/views/notification/_form.php
+++ backend/views/notification/_form.php
@@ -11,35 +11,51 @@ use yii\widgets\ActiveForm;
 <div class="notification-form">
 
     <?php $form = ActiveForm::begin(); ?>
-    <div class="row justify-content-center"><div class='col-md-8'> <?= $form->field($model, 'user_id')->textInput() ?>
+    <div class="row justify-content-center">
+        <div class='col-md-8'> <?= $form->field($model, 'user_id')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'receiver')->textInput(['maxlength' => true]) ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'receiver')->textInput(['maxlength' => true]) ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'type')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'type')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'text')->textarea(['rows' => 6]) ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'created_at')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'created_at')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'created_by')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'created_by')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'updated_at')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'updated_at')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'updated_by')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'updated_by')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'update_at')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'update_at')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'response')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'response')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'priority')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'priority')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'send_at')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'send_at')->textInput() ?>
 
-  </div><div class='col-md-8'> <?= $form->field($model, 'status')->textInput() ?>
+        </div>
+        <div class='col-md-8'> <?= $form->field($model, 'status')->textInput() ?>
 
-  </div>    </div>    <div class="form-group mb-0 card-footer d-flex justify-content-between">
+        </div>
+    </div>
+    <div class="form-group mb-0 card-footer d-flex justify-content-between">
         <div class="col-md-10 d-flex justify-content-end">
-            <button type="submit" class="btn btn-info btn-rounded">ثبت</button>        </div>
+            <button type="submit" class="btn btn-info btn-rounded">ثبت</button>
+        </div>
     </div>
     <?php ActiveForm::end(); ?>
 
