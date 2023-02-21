c0c68ffd072cf8755f73791764d874571aa2d23d
fdaei <nasimdaei8@gmail.com>
2023-02-21 17:36:43 +0330

    event model

---
+++ common/models/Event.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii2tech\ar\softdelete\SoftDeleteBehavior;
+
+/**
+ * This is the model class for table "{{%event}}".
+ *
+ * @property int $id
+ * @property string $title
+ * @property float $price
+ * @property float $price_before_discount
+ * @property string $description
+ * @property string $headlines
+ * @property string $event_times
+ * @property string $address
+ * @property float $longitude
+ * @property float $latitude
+ * @property string $sponsors
+ * @property int $updated_at
+ * @property int $updated_by
+ * @property int $created_at
+ * @property int $created_by
+ * @property int $deleted_at
+ *
+ * @property User $createdBy
+ * @property User $updatedBy
+ */
+class Event extends \yii\db\ActiveRecord
+{
+
+    public static function tableName()
+    {
+        return '{{%event}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'price', 'price_before_discount', 'description', 'headlines', 'event_times', 'address', 'longitude', 'latitude', 'sponsors'], 'required'],
+            [['description', 'address'], 'string'],
+            [['headlines', 'event_times', 'sponsors'], 'safe'],
+            [['title'], 'string', 'max' => 255],
+            [['price','longitude', 'latitude', 'price_before_discount'], 'filter', 'filter' => function ($number) {
+                return Yii::$app->customHelper->toEn($number);
+            }],
+            [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
+            [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['updated_by' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'title' => Yii::t('app', 'Title'),
+            'price' => Yii::t('app', 'Price'),
+            'price_before_discount' => Yii::t('app', 'Price Before Discount'),
+            'description' => Yii::t('app', 'Description'),
+            'headlines' => Yii::t('app', 'Headlines'),
+            'event_times' => Yii::t('app', 'Event Times'),
+            'address' => Yii::t('app', 'Address'),
+            'longitude' => Yii::t('app', 'Longitude'),
+            'latitude' => Yii::t('app', 'Latitude'),
+            'sponsors' => Yii::t('app', 'Sponsors'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+            'updated_by' => Yii::t('app', 'Updated By'),
+            'created_at' => Yii::t('app', 'Created At'),
+            'created_by' => Yii::t('app', 'Created By'),
+            'deleted_at' => Yii::t('app', 'Deleted At'),
+        ];
+    }
+
+    /**
+     * Gets query for [[CreatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getCreatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'created_by']);
+    }
+    public function normalizeNumber($value)
+    {
+        return Yii::$app->customHelper->toEn($value);
+    }
+    /**
+     * Gets query for [[UpdatedBy]].
+     *
+     * @return \yii\db\ActiveQuery|yii\db\ActiveQuery
+     */
+    public function getUpdatedBy()
+    {
+        return $this->hasOne(User::class, ['id' => 'updated_by']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return EventQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        $query = new EventQuery(get_called_class());
+        return $query->active();
+    }
+
+    public function canDelete()
+    {
+        return true;
+    }
+
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class' => TimestampBehavior::class
+            ],
+            [
+                'class' => BlameableBehavior::class,
+                'createdByAttribute' => 'created_by',
+                'updatedByAttribute' => 'updated_by',
+            ],
+            'softDeleteBehavior' => [
+                'class' => SoftDeleteBehavior::class,
+                'softDeleteAttributeValues' => [
+                    'deleted_at' => time(),
+                ],
+                'restoreAttributeValues' => [
+                    'deleted_at' => 0,
+                ],
+                'replaceRegularDelete' => false, // mutate native `delete()` method
+                'invokeDeleteEvents' => false
+            ],
+        ];
+    }
+
+    public function fields()
+    {
+        return [
+            'id',
+            'title',
+            'price',
+            'price_before_discount',
+            'description',
+            'headlines',
+            'event_times',
+            'address',
+            'longitude',
+            'latitude',
+            'sponsors',
+        ];
+    }
+
+    public function extraFields()
+    {
+        return [];
+    }
+}
---
+++ common/models/EventSearch.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace common\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use common\models\Event;
+
+/**
+ * EventSearch represents the model behind the search form of `common\models\Event`.
+ */
+class EventSearch extends Event
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'updated_at', 'updated_by', 'created_at', 'created_by', 'deleted_at'], 'integer'],
+            [['title', 'description', 'headlines', 'event_times', 'address', 'sponsors'], 'safe'],
+            [['price', 'price_before_discount', 'longitude', 'latitude'], 'number'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Event::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'price' => $this->price,
+            'price_before_discount' => $this->price_before_discount,
+            'longitude' => $this->longitude,
+            'latitude' => $this->latitude,
+            'updated_at' => $this->updated_at,
+            'updated_by' => $this->updated_by,
+            'created_at' => $this->created_at,
+            'created_by' => $this->created_by,
+            'deleted_at' => $this->deleted_at,
+        ]);
+
+        $query->andFilterWhere(['like', 'title', $this->title])
+            ->andFilterWhere(['like', 'description', $this->description])
+            ->andFilterWhere(['like', 'headlines', $this->headlines])
+            ->andFilterWhere(['like', 'event_times', $this->event_times])
+            ->andFilterWhere(['like', 'address', $this->address])
+            ->andFilterWhere(['like', 'sponsors', $this->sponsors]);
+
+        return $dataProvider;
+    }
+}
