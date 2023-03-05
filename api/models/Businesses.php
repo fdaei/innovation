37735c1156cc18ff957aa2659f2c6adf522bfeb9
MohmmadJavad Mirzaei <mmrroott@gmail.com>
2023-03-05 13:57:19 +0330

    Businesses api, Needs to be completed

---
+++ api/models/Businesses.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace api\models;
+
+use common\behaviors\CdnUploadImageBehavior;
+use yii\base\Model;
+use yii\behaviors\BlameableBehavior;
+use yii\behaviors\TimestampBehavior;
+use yii2tech\ar\softdelete\SoftDeleteBehavior;
+
+class Businesses extends \common\models\Businesses
+{
+    public function fields()
+    {
+        return [
+            'id',
+            'picture_desktop' => function (self $model) {
+                return $model->getUploadUrl('picture_desktop');
+            },
+            'picture_mobile' => function (self $model) {
+                return $model->getUploadUrl('picture_mobile');
+            },
+            'name',
+            'description_brief',
+            'description',
+            'website',
+            'telegram',
+            'instagram',
+            'whatsapp',
+            'pic_main_desktop',
+            'pic_main_mobile',
+            'pic_small1_desktop',
+            'pic_small1_mobile',
+            'pic_small2_desktop',
+            'pic_small2_mobile',
+            'statistics',
+            'services',
+            'investors',
+        ];
+    }
+}
