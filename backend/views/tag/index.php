57f6a9134b8d588192b763e527f7ee89cca957ef
fdaei <nasimdaei8@gmail.com>
2023-06-22 09:05:29 +0330

    view

---
+++ backend/views/tag/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use common\models\Tag;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+
+/** @var yii\web\View $this */
+/** @var common\models\TagSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Tags';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="event-index card material-card">
+    <div class="card-header d-flex justify-content-between">
+        <h1><?= Html::encode($this->title) ?></h1>
+
+        <p>
+            <?= Html::a(Yii::t('app', 'Create Event'), ['create'], ['class' => 'btn btn-primary']) ?>
+
+        </p>
+    </div>
+    <?php Pjax::begin(); ?>
+    <div class="card-body">
+        <?= $this->render('_search', ['model' => $searchModel]); ?>
+
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'columns' => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'type',
+                'name',
+                'frequency',
+                'status',
+                //'description',
+                [
+                    'class' => ActionColumn::className(),
+                    'urlCreator' => function ($action, Tag $model, $key, $index, $column) {
+                        return Url::toRoute([$action, 'id' => $model->id]);
+                    }
+                ],
+            ],
+        ]); ?>
+    </div>
+    <?php Pjax::end(); ?>
+
+</div>
