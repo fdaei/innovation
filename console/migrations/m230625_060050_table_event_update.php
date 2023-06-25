d277c633edef52b41a890ee9238e5bf35b73800b
baghaee <baghaee.sr@gmail.com>
2023-06-25 09:32:17 +0330

    migration table event

---
+++ console/migrations/m230625_060050_table_event_update.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230625_060050_table_event_update
+ */
+class m230625_060050_table_event_update extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m230625_060050_table_event_update cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m230625_060050_table_event_update cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
