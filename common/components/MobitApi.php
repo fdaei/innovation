48436d910cdc0087a0e428da9d793c45d92ea0e9
baghaee <baghaee.sr@gmail.com>
2023-06-26 12:50:46 +0330

    add component MobitApi

---
+++ common/components/MobitApi.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace common\components;
+
+use yii\base\Component;
+use yii\httpclient\Client;
+
+class MobitApi extends Component
+{
+
+    public static function sendSmsLogin($mobileNumber,$otpCode){
+
+        $client = new Client();
+
+        $url = strtr('https://api.mobit.ir/api/web/v10/developer/verify-sms?company_name={company_name}&code={code}&receptor={receptor}',
+            [
+            '{company_name}' => 'آوینوکس',
+            '{code}' => $otpCode,
+            '{receptor}' => $mobileNumber
+            ]
+        );
+
+        $response = $client->createRequest()
+            ->setMethod('POST')
+            ->setUrl($url)
+            ->send();
+
+        if ($response->isOk) {
+            return true;
+        }
+        return false;
+    }
+}
