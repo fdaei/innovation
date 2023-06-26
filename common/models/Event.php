27cf8fe5a16fd36e0ef2d7ad90bf827911c03070
fdaei <nasimdaei8@gmail.com>
2023-06-26 10:14:57 +0330

    EVENT with scnario

--- common/models/Event.php
+++ common/models/Event.php
@@ -72,7 +72,7 @@ class Event extends \yii\db\ActiveRecord
             [['price', 'longitude', 'latitude', 'price_before_discount'], 'filter', 'filter' => function ($number) {
                 return Yii::$app->customHelper->toEn($number);
             }],
-//            ['picture', 'image', 'minWidth' => 1180, 'maxWidth' => 1180, 'minHeight' => 504, 'maxHeight' => 504, 'extensions' => 'jpg, jpeg, png', 'maxSize' => 1024 * 1024 * 2, 'enableClientValidation' => false],
+            ['picture', 'image', 'extensions' => 'jpg, jpeg, png', 'maxSize' => 1024 * 1024 * 2, 'enableClientValidation' => false],
             [['created_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['created_by' => 'id']],
             [['updated_by'], 'exist', 'skipOnError' => true, 'targetClass' => User::class, 'targetAttribute' => ['updated_by' => 'id']],
         ];
@@ -217,7 +217,7 @@ class Event extends \yii\db\ActiveRecord
             [
                 'class' => CdnUploadImageBehavior::class,
                 'attribute' => 'picture',
-                'scenarios' => [self::SCENARIO_CREATE,self::SCENARIO_UPDATE],
+                'scenarios' => [self::SCENARIO_CREATE,self::SCENARIO_UPDATE,self::SCENARIO_DEFAULT],
                 'instanceByName' => false,
                 //'placeholder' => "/assets/images/default.jpg",
                 'deleteBasePathOnDelete' => false,
