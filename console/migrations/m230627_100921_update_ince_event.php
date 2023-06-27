885734f7ff572fdd3e5b39fda47cae3cda482290
Sadegh Shafii <sadshafiei.01@gmail.com>
2023-06-27 14:55:16 +0330

    Bug Fixed: Event event_times ==> plz build

--- console/migrations/m230627_100921_update_ince_event.php
+++ console/migrations/m230627_100921_update_ince_event.php
@@ -9,7 +9,7 @@ class m230627_100921_update_ince_event extends Migration
 {
     public function safeUp()
     {
-        $this->alterColumn('{{%event}}', 'headlines', $this->json()->null(),);
+        $this->alterColumn('{{%event}}', 'headlines', $this->json()->notNull()->defaultExpression('(JSON_OBJECT())'));
     }
 
     public function safeDown()
