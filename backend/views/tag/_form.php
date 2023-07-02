4e14b96b951756bc843d881a3e2dfeb9399772ab
fdaei <nasimdaei8@gmail.com>
2023-07-02 09:37:49 +0330

    tag view

--- backend/views/tag/_form.php
+++ backend/views/tag/_form.php
@@ -1,5 +1,6 @@
 <?php
 
+use common\models\Tag;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -10,19 +11,17 @@ use yii\widgets\ActiveForm;
 
 <div class="tag-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'type')->textInput() ?>
+    <?php $form = ActiveForm::begin(['id'=>'tag-form']); ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'frequency')->textInput() ?>
+    <?= $form->field($model, 'type')->dropDownList(Tag::itemAlias('Type'), ['prompt' => Yii::t('app', 'Select Tags')]) ?>
 
-    <?= $form->field($model, 'status')->textInput() ?>
+    <?= $form->field($model, 'status')->dropDownList(Tag::itemAlias('Status'), ['prompt' => Yii::t('app', 'Select Status')]) ?>
 
     <?= $form->field($model, 'color')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'additional_data')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
