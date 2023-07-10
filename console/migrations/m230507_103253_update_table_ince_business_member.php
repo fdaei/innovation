0d3eeac033a1699cd82828424e70e8daa2c23ee8
baghaee <baghaee.sr@gmail.com>
2023-07-10 11:31:15 +0330

    edit migration

--- console/migrations/m230507_103253_update_table_ince_business_member.php
+++ console/migrations/m230507_103253_update_table_ince_business_member.php
@@ -6,35 +6,10 @@ class m230507_103253_update_table_ince_business_member extends Migration
 {
     public function safeUp()
     {
-        $this->dropForeignKey('ince_business_member_ibfk_1', '{{%business_member}}');
-        $this->dropIndex('business_id', '{{%business_member}}');
 
-        $this->createIndex('business_id', '{{%business_member}}', ['business_id']);
-
-        $this->addForeignKey(
-            'ince_business_member_ibfk_1',
-            '{{%business_member}}',
-            ['business_id'],
-            '{{%businesses}}',
-            ['id'],
-            'RESTRICT',
-            'RESTRICT'
-        );
     }
-
     public function safeDown()
     {
-        $this->dropForeignKey('ince_business_member_ibfk_1', '{{%business_member}}');
-        $this->dropIndex('business_id', '{{%business_member}}');
-        $this->createIndex('business_id', '{{%business_member}}', ['business_id']);
-        $this->addForeignKey(
-            'ince_business_member_ibfk_1',
-            '{{%business_member}}',
-            ['business_id'],
-            '{{%business}}',
-            ['id'],
-            'RESTRICT',
-            'RESTRICT'
-        );
+
     }
 }
