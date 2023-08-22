759067745e9d15278e3acd26a8cd09cb1c29fe0d
fdaei <nasimdaei8@gmail.com>
2023-08-22 14:49:23 +0330

    search in mentor

--- backend/views/mentors-advice-request/_search.php
+++ backend/views/mentors-advice-request/_search.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
+use yii\helpers\Url;
+use kartik\select2\Select2;
 
 /** @var yii\web\View $this */
 /** @var common\models\MentorsAdviceRequestSearch $model */
@@ -17,32 +19,35 @@ use yii\bootstrap4\ActiveForm;
             'data-pjax' => 1
         ],
     ]); ?>
-<!--    <div class="row"><div class="col-md-3">    --><?php //= $form->field($model, 'id') ?>
+    <div class="row">
+        <div class="col-md-3">
+            <?= $form->field($model, 'mobile') ?>
+        </div>
 
-<!--</div>-->
-<div class="col-md-3">    <?= $form->field($model, 'user_id') ?>
+        <div class="col-md-3">
+            <?= $form->field($model, 'name') ?>
+        </div>
 
-<div class="col-md-3">    <?php //= $form->field($model, 'date_advice') ?>
+        <div class="col-md-3">
+            <?= $form->field($model, 'mentor_id')->widget(Select2::classname(), [
+                'initValueText' => $model->mentor ? $model->mentor->name : '', // Assuming mentor is a relation in your model
+                'options' => ['placeholder' => 'جستجوی مشاور...'],
+                'pluginOptions' => [
+                    'allowClear' => true,
+                    'minimumInputLength' => 2,
+                    'ajax' => [
+                        'url' => Url::to(['/mentor/search-mentors']),
+                        'dataType' => 'json',
+                        'data' => new \yii\web\JsExpression('function(params) { return {q:params.term}; }'),
+                    ],
+                ],
+            ])->label('نام مشاوره'); ?>
+        </div>
+    </div>
 
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'type') ?>
-
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'file') ?>
-
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'status') ?>
-
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'deleted_at') ?>
-
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'updated_by') ?>
-
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'update_at') ?>
-
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'created_at') ?>
-
-</div><div class="col-md-3">    <?php // echo $form->field($model, 'created_by') ?>
-
-</div>    </div>    <div class="form-group">
-        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary ']) ?>
-        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-primary ']) ?>
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('app', 'Search'), ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton(Yii::t('app', 'Reset'), ['class' => 'btn btn-outline-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
